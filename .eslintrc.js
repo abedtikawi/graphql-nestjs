6e102620af4fe7946170ff100eabe4d5aaba6724
AbedTikkawi <abed.tikkawi@360mea.com>
2022-09-05 08:24:43 +0300

    modified eslint & added docker compose for postgres

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,28 @@
+// module.exports = {
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     project: 'tsconfig.json',
+//     sourceType: 'module',
+//     tsconfigRootDir: __dirname,
+//   },
+//   plugins: ['@typescript-eslint/eslint-plugin'],
+//   extends: [
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:prettier/recommended',
+//   ],
+//   root: true,
+//   env: {
+//     node: true,
+//     jest: true,
+//   },
+//   ignorePatterns: ['.eslintrc.js'],
+//   rules: {
+//     '@typescript-eslint/interface-name-prefix': 'off',
+//     '@typescript-eslint/explicit-function-return-type': 'off',
+//     '@typescript-eslint/explicit-module-boundary-types': 'off',
+//     '@typescript-eslint/no-explicit-any': 'off',
+//   },
+// };
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -5,6 +30,7 @@ module.exports = {
     sourceType: 'module',
     tsconfigRootDir: __dirname,
   },
+  indent: ['error', 4],
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
@@ -21,8 +47,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars-experimental': 'error',
-    'no-unused-vars': 'off',
   },
 };
