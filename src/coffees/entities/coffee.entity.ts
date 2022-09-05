a5c751592092af144b6a409bbc8415ab96c1ac5a
AbedTikkawi <abed.tikkawi@360mea.com>
2022-09-05 08:22:03 +0300

    added many to many reference

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,10 +1,25 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
 
+@Entity()
 @ObjectType({ description: 'Coffee Model' })
 export class Coffee {
+  @PrimaryGeneratedColumn()
   @Field(() => ID, { description: 'A unique identifier for coffees' })
   id: number;
+  @Column()
   name: string;
+  @Column()
   brand: string;
-  flavors: string[];
+
+  @JoinTable()
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees, { cascade: true })
+  flavors?: Flavor[];
 }
